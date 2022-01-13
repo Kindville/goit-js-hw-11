ca884d16d549ccd74dfec791dc9caf425c8ee514
Kseniia <kceniawhite@gmail.com>
2022-01-14 00:48:46 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+// import './css/styles.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './fetchImages'
@@ -16,47 +16,47 @@ searchForm.addEventListener('sumbit', onSearchForm)
 loadMore.addEventListener('click', onLoadMoreBtn)
 let query = '';
 let page = 1;
-const perPage = 40;
-let simpleLightbox;
+// const perPage = 40;
 
-function onSearchForm(e) {
-  e.preventDefault();
-  page = 1;
-  query = e.currentTarget.searchQuery.value.trim();
-  gallery.innerHTML = ''
+// function onSearchForm(e) {
+//   e.preventDefault();
+//   page = 1;
+//   query = e.currentTarget.searchQuery.value.trim();
+//   gallery.innerHTML = ''
   
 
-  if (query === '') {
-      Notiflix.Notify.failure('Please specify your search.')
-    return;
-  }
-  fetchPictures(query, page, perPage)
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-      Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-      } else {
-        renderGallery(data.hits)
-        simpleLightbox = new SimpleLightbox('.gallery').refresh()
-         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+//   if (query === '') {
+//       Notiflix.Notify.failure('Please specify your search.')
+//     return;
+//   }
+//   fetchPictures(query, page, perPage)
+//     .then(({ data }) => {
+//       if (data.totalHits === 0) {
+//       Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+//       } else {
+//         renderGallery(data.hits)
+//         simpleLightbox = new SimpleLightbox('.gallery').refresh()
+//          Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
         
-        // if (data.totalHits > perPage) {
-          
-        // }
-    }
-    })
-  .catch(error)
-}
+//         if (data.totalHits > perPage) {
+//           loadMore.classList.remove('is-hidden')
 
-function onLoadMoreBtn() {
-  page += 1;
-  simpleLightbox.destroy()
+//         }
+//     }
+//     })
+//   .catch(error)
+// }
 
-  fetchPictures(query, page, perPage)
-    .then(({ data }) => {
-      renderGallery(data.hits)
-      simpleLightbox = new SimpleLightbox('.gallery').refresh()
-  })
-}
+// function onLoadMoreBtn() {
+//   page += 1;
+//   simpleLightbox.destroy()
+
+//   fetchPictures(query, page, perPage)
+//     .then(({ data }) => {
+//       renderGallery(data.hits)
+//       simpleLightbox = new SimpleLightbox('.gallery').refresh()
+//   })
+// }
 
 function renderGallery(images) {
   const markup = images
