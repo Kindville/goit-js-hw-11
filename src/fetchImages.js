7eb305d0f9a8226514ad5cafaa8016aa217cc7cd
Kseniia <kceniawhite@gmail.com>
2022-01-16 00:46:46 +0200

    add css file

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,26 +1,19 @@
-// import axios from 'axios'
-import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// axios.defaults.baseURL = 'https://pixabay.com/api/'
-// const KEY = '25204764-ad3097bdd52dd29ccb650192a'
+const axios = require('axios').default;
 
-export function fetchPictures(query, page, perPage) {
-   console.log(query, page, perPage);
-  // return response = await axios.get(
-  //   `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-  // )
-  //  console.log(1);     //  &page=${page}&per_page=${perPage}
+axios.defaults.baseURL = 'https://pixabay.com/api/'
+const KEY = '25204764-ad3097bdd52dd29ccb650192a'
+
+export  function fetchPictures(query, page, perPage) {
+   const response =  axios.get(
+     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
+.then(response => response.data)
+  return response;
+
+//    
 
   //  return fetch(`https://pixabay.com/api/?key=25204764-ad3097bdd52dd29ccb650192a&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
-  // return fetch('https://pixabay.com/api/?key=25204764-ad3097bdd52dd29ccb650192a&q=yellow+flowers&image_type=photo&pretty=true')
-  return fetch('https://pixabay.com/api/?key=25204764-ad3097bdd52dd29ccb650192a&q=yellow+flowers&image_type=photo&pretty=true')
-        .then(response => {
-          if (response.status === 404) {
-                 return  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-          }
-          if (response.ok) return response.json();
-          throw new Error('Error fetching data')   
-        })
+  //   .then(response => response.json())
+  //   .then(data =>  data)
 }
 
 
--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ import {fetchPictures } from './fetchImages';
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css'
 
-
 const searchForm = document.querySelector('#search-form')
 const loadMore = document.querySelector('.load-more')
 const gallery = document.querySelector('.gallery');
 
 searchForm.addEventListener('submit',  onSearchForm )
-// console.log(searchForm);
-// loadMore.addEventListener('click', onLoadMoreBtn)
+loadMore.addEventListener('click', onLoadMoreBtn)
+
 let query = '';
 let page = 1;
 const perPage = 40;
-let simpleLightbox
-
+let simpleLightbox;
 
 function onSearchForm(e) {
   e.preventDefault();
@@ -26,59 +24,74 @@ function onSearchForm(e) {
   gallery.innerHTML = ''
   
   if (query === '') {
-    console.log('mistake');
+    // console.log('mistake');
       Notiflix.Notify.failure('Please specify your search.')
     return
   }
-   fetchPictures(query, page, perPage)
-  
-  //   .then(({ data }) => {
-  //     if (data.totalHits === 0) {
-  //     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-  //     } else {
-  //       renderGallery(data.hits)
-  //       simpleLightbox = new SimpleLightbox('.gallery').refresh()
-  //        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+  fetchPictures(query, page, perPage)
+      .then((data ) => { data
+        if (data.totalHits === 0) {
+          Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+        } else {
+          // console.log(data.totalHits);
+        renderGallery(data.hits)
+        simpleLightbox = new SimpleLightbox('.gallery a').refresh()
+         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
         
-  //       if (data.totalHits > perPage) {
-  //         loadMore.classList.remove('is-hidden')
-
-  //       }
-  //   }
-  //   })
-  // .catch(error)
+        if (data.totalHits > perPage) {
+          loadMore.classList.remove('is-hidden')
+        }
+    }
+    })
+  .catch(error => error)
 }
 
+function renderGallery(images) {
+  const markup = images
+    .map(image => {
+      const { largeImageURL, webformatURL, tags, likes, views, comments, downloads }
+        = image
 
+      return `<a class= "photo" href="${largeImageURL}"> 
+          <div class="photo-card">
+          <img class= "gallery-img" src="${webformatURL} " alt="${tags}" loading="lazy" width="310" height="100" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>${downloads}
+            </p>
+          </div>
+        </div>
+          </a>`
+            })
+  .join('')
+    gallery.insertAdjacentHTML('beforeend', markup)
+}
 
+function onLoadMoreBtn() {
+  page += 1;
+  simpleLightbox.destroy()
 
+  fetchPictures(query, page, perPage)
+    .then((data ) => { data
+      renderGallery(data.hits)
+      simpleLightbox = new SimpleLightbox('.gallery', { captionPosition: 'bottom', captionsData: 'alt', captionDelay: 250 }).refresh()
+      simpleLightbox.on('show.simpleLightbox', function (e) {
+        e.preventDefault()
+      })
+      const totalPages = Math.ceil(data.totalHits / perPage)
 
-// function renderGallery(images) {
-//   const markup = images
-//     .map(image => {
-//       const { largeImageURL, webformatURL, tags, likes, views, comments, downloads }
-//         = image
-
-//       return `<a href="${largeImageURL}"> 
-//           <div class="photo-card">
-//           <img src="${webformatURL} " alt="${tags}" loading="lazy" />
-//           <div class="info">
-//             <p class="info-item">
-//               <b>Likes</b>${likes}
-//             </p>
-//             <p class="info-item">
-//               <b>Views</b>${views}
-//             </p>
-//             <p class="info-item">
-//               <b>Comments</b>${comments}
-//             </p>
-//             <p class="info-item">
-//               <b>Downloads</b>${downloads}
-//             </p>
-//           </div>
-//         </div>
-//           </a>`
-//             })
-//   .join('')
-//     gallery.innerHTML(markup)
-// }
+      if (page >= totalPages) {
+        loadMore.classList.add('is-hidden')
+          Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+      }
+  })
+}
